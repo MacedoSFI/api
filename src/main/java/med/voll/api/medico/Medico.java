d0e06b2d13b61d3dada46d27f79798b20c5946d4
felipe.nomade@yahoo.com <felipe.nomade@yahoo.com>
2022-11-17 14:04:29 -0300

    funcionalidade delete para exclusao logica de médicos adicionada

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -31,7 +31,10 @@ public class Medico {
 	@Embedded
 	private Endereco endereco;
 
+	private Boolean ativo;
+
 	public Medico(DadosCadastroMedico dados) {
+		this.ativo = true;
 		this.nome = dados.nome();
 		this.email = dados.email();
 		this.telefone = dados.telefone();
@@ -53,4 +56,8 @@ public class Medico {
 		}
 
     }
+
+	public void excluir() {
+		this.ativo = false;
+	}
 }
